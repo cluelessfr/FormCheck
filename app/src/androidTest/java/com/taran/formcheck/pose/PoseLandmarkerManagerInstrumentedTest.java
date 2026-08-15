35458e39215879176ce529903828c803b335ec09
Taran Prabhu <taranprabhu1@gmail.com>
2026-08-15 13:03:19 -0700

    Added video frame pose detection.

--- app/src/androidTest/java/com/taran/formcheck/pose/PoseLandmarkerManagerInstrumentedTest.java
+++ app/src/androidTest/java/com/taran/formcheck/pose/PoseLandmarkerManagerInstrumentedTest.java
@@ -1,10 +1,16 @@
 package com.taran.formcheck.pose;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.mediapipe.framework.image.BitmapImageBuilder;
+import com.google.mediapipe.framework.image.MPImage;
+import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarkerResult;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -21,6 +27,51 @@ public class PoseLandmarkerManagerInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         PoseLandmarkerManager manager = new PoseLandmarkerManager(appContext);
+        manager.close();
+    }
+
+    @Test
+    public void testNullImage() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        PoseLandmarkerManager manager = new PoseLandmarkerManager(appContext);
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> manager.processDecodedFrame(null, 0));
+
+        manager.close();
+    }
+
+    @Test
+    public void testNegativeTimestamp() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        PoseLandmarkerManager manager = new PoseLandmarkerManager(appContext);
+
+        Bitmap map = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+
+        MPImage image = new BitmapImageBuilder(map).build();
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> manager.processDecodedFrame(image, -1));
+
+        image.close();
+        manager.close();
+    }
+
+    @Test
+    public void testValidFrameReturnsResult() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        PoseLandmarkerManager manager = new PoseLandmarkerManager(appContext);
+
+        Bitmap map = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+
+        MPImage image = new BitmapImageBuilder(map).build();
+
+        PoseLandmarkerResult result = manager.processDecodedFrame(image, 0);
+
+        Assert.assertNotNull(result);
+
+        image.close();
         manager.close();
     }
 }
--- app/src/main/java/com/taran/formcheck/pose/PoseLandmarkerManager.java
+++ app/src/main/java/com/taran/formcheck/pose/PoseLandmarkerManager.java
@@ -2,10 +2,12 @@ package com.taran.formcheck.pose;
 
 import android.content.Context;
 
+import com.google.mediapipe.framework.image.MPImage;
 import com.google.mediapipe.tasks.core.BaseOptions;
 import com.google.mediapipe.tasks.core.Delegate;
 import com.google.mediapipe.tasks.vision.core.RunningMode;
 import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarker;
+import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarkerResult;
 
 public class PoseLandmarkerManager {
 
@@ -29,5 +31,17 @@ public class PoseLandmarkerManager {
 
     public void close() {
         poseLandmarker.close();
+    }
+
+    public PoseLandmarkerResult processDecodedFrame(MPImage image, long timestampMs) {
+        if (image == null) {
+            throw new IllegalArgumentException("Image cannot be null");
+        }
+
+        if (timestampMs < 0) {
+            throw new IllegalArgumentException(("Timestamp cannot be negative"));
+        }
+
+        return poseLandmarker.detectForVideo(image, timestampMs);
     }
 }
